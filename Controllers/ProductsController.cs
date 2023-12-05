2ffc48a110a283fe2c177e7a88e9d60f88c99664
HlibBondarev <Hlib.Bondarev@gmail.com>
2023-12-05 13:17:07 +0200

    Corrected the alignment

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -12,97 +12,97 @@ using ProductsWithRouting.Services;
 
 namespace ProductsWithRouting.Controllers
 {
-	public class ProductsController : Controller
-	{
-		private List<Product> myProducts;
+    public class ProductsController : Controller
+    {
+        private List<Product> myProducts;
 
-		public ProductsController(Data data)
-		{
-			myProducts = data.Products;
-		}
+        public ProductsController(Data data)
+        {
+            myProducts = data.Products;
+        }
 
-		[Route("{controller}/{action}")]
-		[Route("{items}/{action}")]
-		[Route("{controller}")]
-		[Route("{items}")]
-		public IActionResult Index(int filterId, string filtername)
-		{
-			return View(myProducts);
-		}
+        [Route("{controller}/{action}")]
+        [Route("{items}/{action}")]
+        [Route("{controller}")]
+        [Route("{items}")]
+        public IActionResult Index(int filterId, string filtername)
+        {
+            return View(myProducts);
+        }
 
-		public IActionResult View(int id)
-		{
-			if (myProducts.Find(x => x.Id == id) == null)
-				return RedirectToAction("Error", new ProductError(id, "Wrong product Id input: "));
+        public IActionResult View(int id)
+        {
+            if (myProducts.Find(x => x.Id == id) == null)
+                return RedirectToAction("Error", new ProductError(id, "Wrong product Id input: "));
 
-			//Please, add your implementation of the method
-			return View(/*TODO: pass corresponding product here*/);
-		}
+            //Please, add your implementation of the method
+            return View(/*TODO: pass corresponding product here*/);
+        }
 
-		[HttpGet]
-		public IActionResult Edit(int id)
-		{
-			if (myProducts.Find(x => x.Id == id) == null)
-				return RedirectToAction("Error", new ProductError(id, "Wrong Id input: "));
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (myProducts.Find(x => x.Id == id) == null)
+                return RedirectToAction("Error", new ProductError(id, "Wrong Id input: "));
 
-			return View(/*TODO: pass corresponding product here*/);
-		}
-		[HttpPost]
-		public IActionResult Edit(Product product)
-		{
-			//Please, add your implementation of the method
-			return View(/*TODO: pass corresponding product here*/);
-		}
+            return View(/*TODO: pass corresponding product here*/);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            //Please, add your implementation of the method
+            return View(/*TODO: pass corresponding product here*/);
+        }
 
-		[Route("{controller}/{action}")]
-		[HttpPost]
-		public IActionResult Create(Product product)
-		{
-			// Server side validation if client side javascript is disabled
-			if (ModelState.IsValid)
-			{
-				myProducts.Add(product);
-				return RedirectToAction("Index");
-			}
+        [Route("{controller}/{action}")]
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            // Server side validation if client side javascript is disabled
+            if (ModelState.IsValid)
+            {
+                myProducts.Add(product);
+                return RedirectToAction("Index");
+            }
 
-			string errorMessages = "The created product has not been validated.";
-			foreach (var item in ModelState)
-			{
-				if (item.Value.ValidationState == ModelValidationState.Invalid)
-				{
-					errorMessages = $"{errorMessages}\nExceptiion in property {item.Key}:\n";
-					foreach (var error in item.Value.Errors)
-					{
-						errorMessages = $"{errorMessages}{error.ErrorMessage}\n";
-					}
-				}
-			}
-			return RedirectToAction("Error", new ProductError(product.Id, errorMessages));
-		}
+            string errorMessages = "The created product has not been validated.";
+            foreach (var item in ModelState)
+            {
+                if (item.Value.ValidationState == ModelValidationState.Invalid)
+                {
+                    errorMessages = $"{errorMessages}\nExceptiion in property {item.Key}:\n";
+                    foreach (var error in item.Value.Errors)
+                    {
+                        errorMessages = $"{errorMessages}{error.ErrorMessage}\n";
+                    }
+                }
+            }
+            return RedirectToAction("Error", new ProductError(product.Id, errorMessages));
+        }
 
-		[Route("{controller}/{new}")]
-		public IActionResult Create()
-		{
-			return View(new Product() { Id = myProducts.MaxBy(p => p.Id).Id + 1 });
-		}
+        [Route("{controller}/{new}")]
+        public IActionResult Create()
+        {
+            return View(new Product() { Id = myProducts.MaxBy(p => p.Id).Id + 1 });
+        }
 
 
-		[Route("products/delete/{id}")]
-		public IActionResult Delete(int id)
-		{
-			var product = myProducts.Find(x => x.Id == id);
-			if (product == null)
-				return RedirectToAction("Error", new ProductError(id, "No product with this Id was found: "));
+        [Route("products/delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product = myProducts.Find(x => x.Id == id);
+            if (product == null)
+                return RedirectToAction("Error", new ProductError(id, "No product with this Id was found: "));
 
-			myProducts.Remove(product);
+            myProducts.Remove(product);
 
-			return RedirectToAction("Index");
-		}
+            return RedirectToAction("Index");
+        }
 
-		[Route("~/product-error")]
-		public IActionResult Error(ProductError error)
-		{
-			return View(error);
-		}
-	}
+        [Route("~/product-error")]
+        public IActionResult Error(ProductError error)
+        {
+            return View(error);
+        }
+    }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -9,9 +9,9 @@ namespace ProductsWithRouting.Models
     public class Product
     {
         public int Id { get; set; }
-		[Required]
-		public string Name { get; set; }
-		[Required]
-		public string Description { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
     }
 }
